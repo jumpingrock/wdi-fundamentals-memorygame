d0a63c2bc0c7ea32896a75fbe8bc939f14d95094
kennethyeong <42800519+kennethyeong@users.noreply.github.com>
2018-11-16 17:53:23 +0800

    added card logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,28 @@
 console.log("Up and running!");
 
 var cardsInPlay = [];
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank : "Queen",
+    suit : "Hearts",
+    cardImage : "images/queen-of-hearts.png"
+  },
+  {
+    rank : "Queen",
+    suit : "Diamond",
+    cardImage : "images/queen-of-diamonds.png"
+  },
+  {
+    rank : "King",
+    suit : "Hearts",
+    cardImage : "images/king-of-hearts.png"
+  },
+  {
+    rank : "King",
+    suit : "Diamond",
+    cardImage : "images/king-of-diamonds.png"
+  }
+];
 
 var checkForMatch = function () {
   if (cardsInPlay[0] === cardsInPlay[1]){
@@ -10,11 +31,13 @@ var checkForMatch = function () {
   else if (cardsInPlay[0] != cardsInPlay[1]){
     console.log("Sorry, try again.");
   }
-}
+};
 
 var flipCard = function (cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
-  console.log(cardsInPlay)
-  if (cardsInPlay.length >= 2) {checkForMatch()}
-}
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cardsInPlay);
+  if (cardsInPlay.length === 2) {checkForMatch()}
+};
